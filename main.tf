df8eea7d912c878dd37edc3e910c33566aab3946
arunmodgil <Arun.Kumar1@digital.homeoffice.gov.uk>
2023-10-05 10:26:45 +0100

    Updating TF provider to version 4 ACPENG-670

--- main.tf
+++ main.tf
@@ -3,36 +3,35 @@ resource "aws_elasticache_subnet_group" "elasticache_redis_cluster" {
   subnet_ids = var.subnet_ids
 }
 
+
 resource "aws_elasticache_replication_group" "elasticache_redis_cluster" {
-  at_rest_encryption_enabled    = var.at_rest_encryption_enabled
-  auth_token                    = var.auth_token != "" ? var.auth_token : null
-  auto_minor_version_upgrade    = var.auto_minor_version_upgrade
-  automatic_failover_enabled    = var.automatic_failover_enabled
-  engine_version                = var.engine_version
-  kms_key_id                    = var.kms_key_id != "" ? var.kms_key_id : null
-  maintenance_window            = var.maintenance_window
-  multi_az_enabled              = var.multi_az_enabled
-  node_type                     = var.node_type
-  notification_topic_arn        = var.notification_topic_arn != "" ? var.notification_topic_arn : null
-  number_cache_clusters         = var.number_of_nodes != "" ? var.number_of_nodes : null
-  parameter_group_name          = var.parameter_group_name
-  port                          = 6379
-  replication_group_description = "Replication group for the ${var.name} cluster"
-  replication_group_id          = "${var.name}-rg"
-  security_group_ids            = [aws_security_group.elasticache_redis_cluster.id]
-  snapshot_name                 = var.snapshot_name
-  snapshot_retention_limit      = var.snapshot_retention_limit != "" ? var.snapshot_retention_limit : null
-  snapshot_window               = var.snapshot_window != "" ? var.snapshot_window : null
-  subnet_group_name             = aws_elasticache_subnet_group.elasticache_redis_cluster.name
-  transit_encryption_enabled    = var.transit_encryption_enabled
-  # Cluster mode configuration
-  dynamic "cluster_mode" {
-    for_each = var.automatic_failover_enabled != false ? [1] : []
-    content {
-      replicas_per_node_group = var.replicas_per_node_group
-      num_node_groups         = var.num_node_groups
-    }
-  }
+
+
+
+  at_rest_encryption_enabled = var.at_rest_encryption_enabled
+  auth_token                 = var.auth_token != "" ? var.auth_token : null
+  auto_minor_version_upgrade = var.auto_minor_version_upgrade
+  automatic_failover_enabled = var.automatic_failover_enabled
+  engine_version             = var.engine_version
+  kms_key_id                 = var.kms_key_id != "" ? var.kms_key_id : null
+  maintenance_window         = var.maintenance_window
+  multi_az_enabled           = var.multi_az_enabled
+  node_type                  = var.node_type
+  notification_topic_arn     = var.notification_topic_arn != "" ? var.notification_topic_arn : null
+  num_cache_clusters         = var.number_of_nodes != "" ? var.number_of_nodes : null
+  parameter_group_name       = var.parameter_group_name
+  port                       = 6379
+  description                = "Replication group for the ${var.name} cluster"
+  replication_group_id       = "${var.name}-rg"
+  security_group_ids         = [aws_security_group.elasticache_redis_cluster.id]
+  snapshot_name              = var.snapshot_name
+  snapshot_retention_limit   = var.snapshot_retention_limit != "" ? var.snapshot_retention_limit : null
+  snapshot_window            = var.snapshot_window != "" ? var.snapshot_window : null
+  subnet_group_name          = aws_elasticache_subnet_group.elasticache_redis_cluster.name
+  transit_encryption_enabled = var.transit_encryption_enabled
+
+  num_node_groups         = var.automatic_failover_enabled ? var.num_node_groups : null
+  replicas_per_node_group = var.automatic_failover_enabled ? var.replicas_per_node_group : null
 
   tags = merge(
     var.tags,
